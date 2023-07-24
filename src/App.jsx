2aed698563756d7e74fab8951a6f68a2c3eadd83
Marco <tignanellimarco@gmail.com>
2023-07-24 19:18:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Octokit } from "octokit";
+
+function App() {
+
+  const octokit = new Octokit({
+    auth: 'YOUR-TOKEN'
+  });
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
