b9d2c6d249e6a08b983b336eaf57a39ab250ec70
Marco <tignanellimarco@gmail.com>
2023-07-26 20:15:59 -0300

    set border-box

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -41,18 +41,18 @@ export default function Card({ resource }) {
                 {resource.owner ?
                     <img
                         className="stats-chart"
-                        src={`https://github-readme-stats.vercel.app/api/pin/?username=${resource.owner.name}&repo=${resource.name}&show_owner=true&theme=dark&border_color=141414`}
+                        src={`https://github-readme-stats.vercel.app/api/pin/?username=${resource.owner.name}&repo=${resource.name}&show_owner=true`}
                         alt="Repo stats chart" /> :
 
                     <div className={`charts ${toggleStats ? "translated-charts" : "untranslated-charts"}`}>
                         <img
                             className="stats-chart"
-                            src={`https://github-readme-stats.vercel.app/api?username=${resource.name}&include_all_commits=true&line_height=24&theme=dark&border_color=141414`}
+                            src={`https://github-readme-stats.vercel.app/api?username=${resource.name}&include_all_commits=true&line_height=24`}
                             alt="User stats chart" />
 
                         <img
                             className="stats-chart"
-                            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${resource.name}&layout=compact&card_width=390&theme=dark&border_color=141414`}
+                            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${resource.name}&layout=compact&card_width=390`}
                             alt="User stats chart" />
                     </div>
                 }
