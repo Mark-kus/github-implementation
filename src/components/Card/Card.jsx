99eaa6ac06a6414d1e608ef99c82705a24cbf00f
Marco <tignanellimarco@gmail.com>
2023-07-26 14:54:25 -0300

    fix toggle-button position

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -19,8 +19,19 @@ export default function Card({ resource }) {
 
                 <div>
                     <h3 className="my-0"><a href={resource.url}>{resource.name}</a></h3>
-                    {resource.owner && <p className="my-0 mute"><a href={resource.owner.url}>{resource.owner.name}</a></p>}
+                    <p className="my-0 mute">
+                        {resource.owner ?
+                            <a href={resource.owner.url}>{resource.owner.name}</a> :
+                            resource.location
+                        }
+                    </p>
                 </div>
+
+                <button
+                        className={`base-toggle ${toggleStats ? "active-toggle" : "default-toggle"}`}
+                        onClick={() => setToggleStats(!toggleStats)}>
+                        <div className={`base-dot ${toggleStats ? "active-dot" : "default-dot"}`} />
+                </button>
             </div>
 
             <div id="timestamps">
@@ -30,27 +41,12 @@ export default function Card({ resource }) {
             </div>
 
             {resource.owner ?
-                <p className="my-0">Language: {resource.language}</p> :
-                <p className="my-0">Location: {resource.location}</p>}
-
-            <p>{resource.description}</p>
-
-            {resource.owner ?
                 <img
                     className="stats-chart"
                     src={`https://github-readme-stats.vercel.app/api/pin/?username=${resource.owner.name}&repo=${resource.name}&show_owner=true`}
                     alt="User/repo stats chart" /> :
 
                 <div id="stats-container">
-                    <p className="my-0">
-                        Stats
-                        <button
-                            className={`base-toggle`}
-                            onClick={() => setToggleStats(!toggleStats)}>
-                            <div className={`base-dot ${toggleStats ? "active-dot" : "default-dot"}`} />
-                        </button>
-                    </p>
-
                     {toggleStats ?
 
                         <img
@@ -63,7 +59,6 @@ export default function Card({ resource }) {
                             src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${resource.name}&layout=compact`}
                             alt="User/repo stats chart" />
                     }
-
                 </div>}
 
         </article>
