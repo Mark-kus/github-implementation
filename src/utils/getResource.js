b3ac60f8abb368ef8455319dbc09ad48ac966ca7
Marco <tignanellimarco@gmail.com>
2023-07-26 20:15:43 -0300

    promisify requests

--- src/utils/getResource.js
+++ src/utils/getResource.js
@@ -1,43 +1,20 @@
-export const getUser = async (octokit, username) => {
+export const getUser = (octokit, username) => {
     // Gets user data
-    const res = await octokit.request('GET /users/{username}', {
+    return octokit.request('GET /users/{username}', {
         username,
         headers: {
             'X-GitHub-Api-Version': '2022-11-28'
         }
     })
-    // Formats user data for later use
-    return {
-        image: res.data.avatar_url,
-        url: res.data.html_url,
-        name: res.data.login,
-        description: res.data.bio ?? "No description provided",
-        updatedAt: new Date(res.data.updated_at).toLocaleDateString(),
-        createdAt: new Date(res.data.created_at).toLocaleDateString(),
-        location: res.data.location,
-    }
 }
 
-export const getRepo = async (octokit, owner, repo) => {
+export const getRepo = (octokit, owner, repo) => {
     // Gets repo data
-    const res = await octokit.request('GET /repos/{owner}/{repo}', {
+    return octokit.request('GET /repos/{owner}/{repo}', {
         owner,
         repo,
         headers: {
             'X-GitHub-Api-Version': '2022-11-28'
         }
     })
-    // Formats repo data for later use
-    return {
-        image: res.data.owner.avatar_url,
-        url: res.data.svn_url,
-        name: res.data.name,
-        updatedAt: new Date(res.data.updated_at).toLocaleDateString(),
-        createdAt: new Date(res.data.created_at).toLocaleDateString(),
-        language: res.data.language,
-        owner: {
-            name: res.data.owner.login,
-            url: res.data.owner.html_url,
-        }
-    }
 }
